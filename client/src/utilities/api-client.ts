a58945bb3de9899ca593ede84dc6f2dc275ca798
chenAmrani <chenamrani5@gmail.com>
2024-09-24 20:57:58 +0300

    update

--- client/src/utilities/api-client.ts
+++ client/src/utilities/api-client.ts
@@ -3,6 +3,8 @@ import { io, Socket } from 'socket.io-client';
 let socket: Socket;
 
 export const initSocketConnection = () => {
-     socket = io(`https://moveo-task-backend.vercel.app`); 
+  socket = io(`https://moveo-task-backend.vercel.app`, {
+    transports: ['polling'], // Force polling since WebSockets are not supported
+  });
   return socket;
 };
